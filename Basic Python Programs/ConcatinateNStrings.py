6462c3227149ba1b3218b90c660e7d12d51d8ce7
vivekshelke712 <vivekshelke006@gmail.com>
2025-03-03 21:05:19 +0530

    Module 1 Completed

---
+++ Basic Python Programs/ConcatinateNStrings.py
@@ -0,0 +1,5 @@
+str1 = input("enter String 1 ")
+str2 = input("Enter String 2")
+str3 = input("Enter String 3")
+concatinated_String = str1 + " " + str2 + " " + str3 
+print(concatinated_String)
---
+++ Basic Python Programs/EvenOdd.py
@@ -0,0 +1,5 @@
+Num = int(input("Enter a Number"))
+if(Num % 2 == 0) :
+    print("Number is even")
+else :
+    print("number is Odd")
